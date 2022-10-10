dd910d74341fa1a4d8de3913ad9cc70caa9c2b0e
Gabriel Sardina <gabinex15@gmail.com>
2022-10-10 13:27:51 -0300

    add header component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+	// const [count, setCount] = useState(0);
+
+	return (
+		<div className="App">
+			{/* <h1>App Component</h1>
+			<button>Hola</button> */}
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+import styles from "./header.module.css";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+
+export const Header = () => {
+	return (
+		<header className={styles.header}>
+			<h1>
+				📝
+				<span className={styles.colorTitle_1}>To</span>
+				<span className={styles.colorTitle_2}>Do</span>
+			</h1>
+
+			<form className={styles.newTaskForm}>
+				<input type="text" placeholder="add a new task" />
+				<button>
+					Create
+					<AiOutlinePlusCircle size={20} />
+				</button>
+			</form>
+		</header>
+	);
+};
